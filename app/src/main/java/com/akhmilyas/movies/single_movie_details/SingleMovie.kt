e3b649ad5e45394acf5e39695a1f7d22cc5a3582
mstoops <mstoops@student.21-school.ru>
2020-07-07 12:20:17 +0400

    SingleMovie activity

---
+++ app/src/main/java/com/akhmilyas/movies/single_movie_details/SingleMovie.kt
@@ -0,0 +1,12 @@
+package com.akhmilyas.movies.single_movie_details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.akhmilyas.movies.R
+
+class SingleMovie : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_single_movie)
+    }
+}
