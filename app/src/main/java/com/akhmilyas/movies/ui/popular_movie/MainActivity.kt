7bb653a01f59172df6028e6dff3d00cca8f40293
mstoops <mstoops@student.21-school.ru>
2020-07-17 19:55:22 +0300

    Финальная версия MainActivity

--- app/src/main/java/com/akhmilyas/movies/ui/popular_movie/MainActivity.kt
+++ app/src/main/java/com/akhmilyas/movies/ui/popular_movie/MainActivity.kt
@@ -3,12 +3,69 @@ package com.akhmilyas.movies.ui.popular_movie
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.akhmilyas.movies.R
+import com.akhmilyas.movies.data.api.TheMovieDBClient
+import com.akhmilyas.movies.data.api.TheMovieDBInterface
+import com.akhmilyas.movies.data.repository.NetworkState
 import com.akhmilyas.movies.ui.single_movie_details.SingleMovie
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    lateinit var movieRepository: MoviePagedListRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService : TheMovieDBInterface = TheMovieDBClient.getClient()
+
+        movieRepository = MoviePagedListRepository(apiService)
+
+        viewModel = getViewModel()
+
+        val movieAdapter = PopularMoviePagedListAdapter(this)
+
+        val gridLayoutManager = GridLayoutManager(this, 3)
+        gridLayoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                val viewType = movieAdapter.getItemViewType(position)
+                if (viewType == movieAdapter.MOVIE_VIEW_TYPE) return 1
+                else return 3
+            }
+        }
+
+        popular_recycler_view.layoutManager = gridLayoutManager
+        popular_recycler_view.setHasFixedSize(true)
+        popular_recycler_view.adapter = movieAdapter
+
+        viewModel.moviePagedList.observe(this, Observer {
+            movieAdapter.submitList(it)
+        })
+
+        viewModel.networkState.observe(this, Observer {
+            popular_progress_bar.visibility = if (viewModel.listIsEmpty() && it == NetworkState.LOADING) View.VISIBLE else View.GONE
+            popular_txt_error.visibility = if (viewModel.listIsEmpty() && it == NetworkState.ERROR) View.VISIBLE else View.GONE
+
+            if (!viewModel.listIsEmpty()) {
+                movieAdapter.setNetworkState(it)
+            }
+        })
+    }
+
+    private fun getViewModel(): MainActivityViewModel {
+        return ViewModelProvider(this, object: ViewModelProvider.Factory {
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                @Suppress("UNCHECKED_CAST")
+                return MainActivityViewModel(movieRepository) as T
+            }
+        })[MainActivityViewModel::class.java]
     }
 }
